9ae2acfc60d6ce7ff606a3cefbb8f431ffcf7774
D. Scott Boggs <scott@tams.tech>
2018-10-20 19:03:35 -0400

    parallelism

--- src/routes/routes.cr
+++ src/routes/routes.cr
@@ -1,37 +1,32 @@
 require "kemal"
+require "parallel"
 require "./route_helpers"
 
 module Flix
   extend self
 
   def serve_up
-    get "/ping" do
-      "pong"
-    end
+    8.times do |i|
+      pspawn do
+        get "/ping" { "pong" }
+        get "/dmp" { config.dirs.to_json.tap { |o| Flix.logger.debug "dumping JSON #{o}" } }
+        get "/img/:id" { |env| serve_image env }
+        get "/img" { |env| serve_image env }
+        get "/vid/:id" { |env| serve_video env }
+        get "/vid" { |env| serve_video env }
+        get "/" { |env| env.redirect "/index.html" }
 
-    get "/dmp" do
-      config.dirs.to_json
-    end
-
-    get "/img/:id" { |env| serve_image env }
-    get "/img" { |env| serve_image env }
-    get "/vid/:id" { |env| serve_video env }
-    get "/vid" { |env| serve_video env }
-
-    get "/" { |env| env.redirect "/index.html" }
-
-    public_folder config.webroot
-
-    Kemal.config do |conf|
-      conf.port = Flix.config.port.to_i
-    end
-    begin
-      Kemal.run
-    rescue e : Errno
-      if e.errno == Errno::EADDRINUSE
-        Flix.logger.error "port #{Kemal.config.port} is in use.\n"
+        public_folder config.webroot
+        #
+        # Kemal.config do |conf|
+        #   conf.port = Flix.config.port.to_i
+        # end
+        Kemal.run do |conf|
+          server = conf.server
+          raise "nil server in process #{i}!" if server.nil?
+          server.not_nil!.bind_tcp("0.0.0.0", Flix.config.port.to_i, reuse_port: true)
+        end
       end
-      raise e
     end
   end
 end
