aa23285ec3c790c17549cff47187f6b05b3f0e1b
D. Scott Boggs <scott@tams.tech>
2018-12-31 06:44:45 -0500

    updated deployment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-function create_first_user() {
+create_first_user() {
   user="${flix_first_user:-admin}"
   password="${flix_first_user_password:-`crystal eval 'puts Random::Secure.base64.strip "="'`}"
   echo "no users found, creating first user '$user' with password '$password'."
@@ -11,12 +11,13 @@ function create_first_user() {
 }
 
 flix_dir=$(dirname $(realpath $0))
-flix_config="${flix_config:-/config}"
+export flix_config="${flix_config:-/config}"
 auth_file="$flix_config/users.auth"
 
 cd $flix_dir
 [ -f $auth_file ] || create_first_user $auth_file
-[ "$run_specs" ] && crystal spec
+# run the tests if the $run_specs environment variable is set
+[ -z ${run_specs+is_set} ] || crystal spec
 
 flix \
   --dir "${flix_media_dir:-/media}" \
