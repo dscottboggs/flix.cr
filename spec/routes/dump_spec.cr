f8cc7496ea7ad8a1c53032ce17eb7a89be96ad89
D. Scott Boggs <scott@tams.tech>
2019-01-21 20:36:56 -0500

    Improved filename -> title interpetation heuristics

--- spec/routes/dump_spec.cr
+++ spec/routes/dump_spec.cr
@@ -3,7 +3,7 @@ require "../routes_spec"
 FilePathHashes = {TestVideo: Flix::Scanner.hash(VIDEO_ONE_PATH), TestVideo2: Flix::Scanner.hash(VIDEO_TWO_PATH)}
 EXPECTED_DUMP  = [
   {
-    "title"                     => "media",
+    "title"                     => "Media",
     FilePathHashes[:TestVideo2] => "Test Video - 2",
     FilePathHashes[:TestVideo]  => "Test Video",
   },
