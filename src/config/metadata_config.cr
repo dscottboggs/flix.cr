680037153e645f5d6af04ed9a1137fd31b53c6ba
D. Scott Boggs <scott@tams.tech>
2019-01-31 23:02:13 -0500

    more docs

--- src/config/metadata_config.cr
+++ src/config/metadata_config.cr
@@ -24,6 +24,19 @@ module Flix
 
     extend self
 
+    # This method reads in any new data from the config file, and merges it with
+    # the existing data. Since there are some common false positive situations
+    # one can run into when this method is run, it ignores certain errors. As
+    # such, it's technically possible for you to overwrite your override config
+    # file with one containing the old data. It shouldn't really be possible
+    # though, because the only ignored errors are:
+    #  - When invalid YAML occurs within the first line and first 10 letters
+    #    width-wise of the YAML file; **OR**
+    #  - When the file does not already exist.
+    # If you encounter a situation where the config file is overwritten by the
+    # existing state which isn't one of these situations, or in which these
+    # situations cause a legitimate error to be ignored, please raise an issue
+    # at https://github.com/dscottboggs/flix.cr/issues/new.
     def synchronize!
       cfg = ConfigFile.from_yaml Flix.config.metadata_file
       Flix.config.dirs.each do |dir|
@@ -33,8 +46,7 @@ module Flix
       # A YAML::ParseException will be raised if the file (or in the case of
       # testing, IO) is empty. We should bail here for any other parse
       # exceptions, because if we don't the next step is to overwrite it.
-      p! error
-      unless (error.line_number < 10) && (error.column_number < 10)
+      unless (error.line_number < 1) && (error.column_number < 10)
         raise YAML::ParseException.new(
           "while reading in <<-YAML\n#{Flix.config.metadata_file.rewind.gets_to_end}\nYAML\n#{error.message}",
           line_number: error.line_number,
