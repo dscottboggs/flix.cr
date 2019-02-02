5d303f7e73ee3fa6bb586d64dbc9d06f000c786f
D. Scott Boggs <scott@tams.tech>
2019-02-01 20:46:48 -0500

    cleaned up synchronize!() method and added more tests.

--- spec/scanner/config_data/metadata_config_spec.cr
+++ spec/scanner/config_data/metadata_config_spec.cr
@@ -73,4 +73,71 @@ describe Flix::MetadataConfig do
   ensure
     reset_metadata_file
   end
+
+  it "throws an exception when invalid YAML is encountered" do
+    Flix::MetadataConfig.synchronize!
+    # save the original state
+    old_state = IO::Memory.new Flix.config.metadata_file.to_s
+    # set up some invalid YAML
+    modified = <<-YAML
+    ---
+    folders:
+      E9zk2zyWEDo: invalid yaml here: }
+        title: Media
+        content:
+          m8eHp5iFD88:
+            title: Test Video - 2
+          9UUlUbVCDPY:
+            title: Updated title!
+            thumbnail: /home/scott/Documents/code/flix/test_data/media/TestVideo.jpg
+
+    YAML
+    # clear out the current state
+    if (mf = Flix.config.metadata_file).is_a? IO::Memory
+      mf.clear
+    else
+      fail "got unexpectedly typed metadata_file: #{Flix.config.metadata_file.class}"
+    end
+    # so we can set the modified to the virtual config file
+    modified.to_s io: Flix.config.metadata_file
+    Flix.config.metadata_file.rewind
+    # here's what we're actually testing for
+    expect_raises YAML::ParseException do
+      Flix::MetadataConfig.synchronize!
+    end
+    # and make sure that the invalid state wasn't written
+    Flix.config.metadata_file.rewind.gets_to_end.should eq old_state.gets_to_end
+  end
+  it "ignores the error when invalid YAML is encountered at the beginning of the file" do
+    Flix::MetadataConfig.synchronize!
+    # save the original state
+    old_state = IO::Memory.new Flix.config.metadata_file.to_s
+    # set up some invalid YAML
+    modified = <<-YAML
+    } <<-- bad yaml
+    folders:
+      E9zk2zyWEDo: invalid yaml here: }
+        title: Media
+        content:
+          m8eHp5iFD88:
+            title: Test Video - 2
+          9UUlUbVCDPY:
+            title: Updated title!
+            thumbnail: /home/scott/Documents/code/flix/test_data/media/TestVideo.jpg
+
+    YAML
+    # clear out the current state
+    if (mf = Flix.config.metadata_file).is_a? IO::Memory
+      mf.clear
+    else
+      fail "got unexpectedly typed metadata_file: #{Flix.config.metadata_file.class}"
+    end
+    # so we can set the modified to the virtual config file
+    modified.to_s io: Flix.config.metadata_file
+    Flix.config.metadata_file.rewind
+    # here's what we're actually testing for -- this shouldn't raise.
+    Flix::MetadataConfig.synchronize!
+    # and make sure that the invalid state wasn't written
+    Flix.config.metadata_file.rewind.gets_to_end.should eq old_state.gets_to_end
+  end
 end
--- src/config/metadata_config.cr
+++ src/config/metadata_config.cr
@@ -30,25 +30,37 @@ module Flix
     # such, it's technically possible for you to overwrite your override config
     # file with one containing the old data. It shouldn't really be possible
     # though, because the only ignored errors are:
-    #  - When invalid YAML occurs within the first line and first 10 letters
+    #  - When invalid YAML occurs within the first line and first 9 letters
     #    width-wise of the YAML file; **OR**
     #  - When the file does not already exist.
     # If you encounter a situation where the config file is overwritten by the
     # existing state which isn't one of these situations, or in which these
     # situations cause a legitimate error to be ignored, please raise an issue
     # at https://github.com/dscottboggs/flix.cr/issues/new.
+    #
+    # The rationale for choosing these error cases to ignore is that the
+    # beginning of the config file is always either "---\nfolders:\n" or
+    # "folders:\n", the shorter of which is 9 letters long including the
+    # newline.
+    #
+    # If a YAML parsing error occurs, an exception WILL be thrown, so for every
+    # case except the inital synchronize after launch, be sure to add a resuce
+    # block. We don't want the whole app crashing if invalid YAML is encountered
+    # by a remotely-triggered metadata refresh.
     def synchronize!
-      cfg = ConfigFile.from_yaml Flix.config.metadata_file
-      Flix.config.dirs.each do |dir|
-        dir.merge! cfg.folders[dir.hash]
-      end
+      read_in
     rescue error : YAML::ParseException
       # A YAML::ParseException will be raised if the file (or in the case of
       # testing, IO) is empty. We should bail here for any other parse
       # exceptions, because if we don't the next step is to overwrite it.
-      unless (error.line_number < 1) && (error.column_number < 10)
+      unless (error.line_number < 2) && (error.column_number < 10)
         raise YAML::ParseException.new(
-          "while reading in <<-YAML\n#{Flix.config.metadata_file.rewind.gets_to_end}\nYAML\n#{error.message}",
+          String.build do |emsg|
+            emsg << "while reading in << YAML\n"
+            emsg << Flix.config.metadata_file.rewind.gets_to_end
+            emsg << "\nYAML\ngot error:\n"
+            emsg << error.message
+          end,
           line_number: error.line_number,
           column_number: error.column_number)
       end
@@ -58,19 +70,29 @@ module Flix
       # actually a problem and should be raised.
       raise error unless error.errno === Errno::ENOENT
     ensure
-      begin
-        Flix.config.metadata_file.rewind
-        ConfigFile.new(dirs: Flix.config.dirs).to_yaml Flix.config.metadata_file
-      ensure
-        Flix.config.metadata_file.close unless Flix.config.testing?
+      write_current_state
+    end
+
+    private def read_in
+      cfg = ConfigFile.from_yaml Flix.config.metadata_file
+      Flix.config.dirs.each do |dir|
+        dir.merge! cfg.folders[dir.hash]
       end
+    end
+
+    def write_current_state
+      mf = Flix.config.metadata_file mode: "w"
+      mf.rewind
+      if mf.responds_to? :truncate
+        mf.truncate
+      elsif mf.responds_to? :clear
+        mf.clear
+      else
+        raise "Metadata file is not able to be truncated." if Flix.config.testing?
+      end
+      ConfigFile.new(dirs: Flix.config.dirs).to_yaml mf
+    ensure
+      Flix.config.metadata_file(mode: "w").close unless Flix.config.testing?
     end
   end
 end
-
-# Hash(String, Flix::Scanner::FileMetadata::ConfigData).from_json(file).folders.each do |id, conf|
-#   Flix
-#   .config
-#   .dirs
-#   .find? { |dir| dir.hash === id }
-#   .try(&.merge conf)
