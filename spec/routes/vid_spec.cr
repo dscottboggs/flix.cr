c10d77ec6c7e8202ba8fc2115693f89b6f322924
D. Scott Boggs <scott@tams.tech>
2019-02-02 12:04:42 -0500

    Crystal 0.27.1 disallows nested it statements.
    I see the rationale as well - shared data between test cases should be 
    minimized or eliminated, and a test for a subcompenent shouldn't fail 
    because the parent component does.

--- spec/routes/vid_spec.cr
+++ spec/routes/vid_spec.cr
@@ -4,11 +4,19 @@ describe "/vid" do
   it "streams a video found in the /dmp results" do
     get "/dmp"
     response.status_code.should eq 200
-    id = Array(Hash(String, String)).from_json(response.body).first.reject("title", "thumbnail").keys.first
+    id = Array(Hash(String, String))
+      .from_json(response.body)
+      .first
+      .reject("title", "thumbnail")
+      .keys
+      .first
+    # pp! Flix.config.all_videos.keys
     get "/vid/#{id}"
     response.status_code.should eq 200
-    it "responds with the expected headers" do
-      response.headers["Content-Type"]?.should eq "application/octet-stream"
-    end
+  end
+  it "responds with the expected headers" do
+    get "/dmp"
+    response.status_code.should eq 200
+    response.headers["Content-Type"]?.should eq "application/octet-stream"
   end
 end
--- spec/scanner/file_spec.cr
+++ spec/scanner/file_spec.cr
@@ -1,14 +1,18 @@
 require "../spec_helper"
 
 describe "mime_type" do
-  it "reports its mime type" do
-    context (file = TEST_FILES[:video_one]) do
+  context (file = TEST_FILES[:video_one]) do
+    it "reports its mime type" do
       Flix::Scanner::MimeType.of(file.path).should eq Flix::Scanner::MimeType::MP4
     end
-    context (file = TEST_FILES[:video_two]) do
+  end
+  context (file = TEST_FILES[:video_two]) do
+    it "reports its mime type" do
       Flix::Scanner::MimeType.of(file.path).should eq Flix::Scanner::MimeType::MP4
     end
-    context (file = TEST_FILES[:image_one]) do
+  end
+  context (file = TEST_FILES[:image_one]) do
+    it "reports its mime type" do
       Flix::Scanner::MimeType.of(file.path).should eq Flix::Scanner::MimeType::JPEG
     end
   end
